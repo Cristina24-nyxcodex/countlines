761fe143ed4ad42b7683bbdd245173ff60f02f00
nyxcodex24 <cristinatorredemer@gmail.com>
2025-11-05 21:22:22 +0100

    second commit

--- countlines.sh
+++ countlines.sh
@@ -1,2 +1,2 @@
 lines=$( cat $1 | wc -l )
-if [[ $lines -eq 0 ]]; then echo $1 has zero lines; elif [[ $lines -eq 1 ]]; then echo $1 has one line; else echo $1 has more than one line ($lines lines); fi 
+if [[ $lines -eq 0 ]]; then echo $1 has zero lines; elif [[ $lines -eq 1 ]]; then echo $1 has one line; else echo $1 has more than one line $lines lines; fi 
